63beeaeb05c0e5ebe65860da9a1a54e48c76e221
Rahul Raj <rahulrajshopping@icloud.com>
2019-01-31 12:19:29 -0500

    added package.json and hid

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,25 +1,25 @@
 const friends = [
     {
         "name": "Harry Potter",
-        "photo": "../../images/harrypotter_profile.jpg",
+        "photo": "images/harrypotter_profile.jpg",
         "scores":[5, 3, 2, 1, 3, 2, 5, 1, 3, 5]
     }, 
 
     {
         "name": "Ron Weasley",
-        "photo": "../../images/ronweasley_profile.jpg",
+        "photo": "images/ronweasley_profile.jpg",
         "scores":[2, 4, 1, 4, 1, 1, 4, 4, 4, 5]
     }, 
 
     {
         "name": "Hermione Granger",
-        "photo": "../../images/hermionegranger_profile.jpg",
+        "photo": "images/hermionegranger_profile.jpg",
         "scores":[3, 5, 5, 1, 1, 3, 4, 3, 3, 5]
     }, 
 
     {
         "name": "Lord Voldemort",
-        "photo": "../../images/voldemort_profile.jpg",
+        "photo": "images/voldemort_profile.jpg",
         "scores":[3, 1, 3, 1, 1, 5, 5, 1, 1, 1]
     }    
 ]
